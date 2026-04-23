a31a07b5127f68e04588c622667e33b8b6c10ac9
sofhatusolihah <sofhatusolihah@gmial.com>
2026-04-23 11:52:10 +0700

    first commit

---
+++ index1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>border="1" width="100%"
+        <tr>
+            <td>width="10%"
+                <img src="https://unsiq.ac.id/visi" alt="unsiq",width"100", hight"100">
+            </td>
+            <td width"90%">
+                <h2>Selamat datang dimata kuliah Pemrograman Web</h2>
+                <h3>Pertemuan ke 2</h3>
+            </td>
+        </tr>
+
+    </table>
+    <hr>
+    <!-- paragraf -->
+     <p align="center"> Mata kuliah ini memperknalkan</p>
+</body>
+</html>
